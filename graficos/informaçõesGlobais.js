aa5f6d02b0f684f6b96e19f9d6c227cc196f9c97
MilenaLuiza <128715641+MilenaLuiza@users.noreply.github.com>
2024-10-23 07:47:30 -0300

    .

--- graficos/informaçõesGlobais.js
+++ graficos/informaçõesGlobais.js
@@ -1,1 +1,9 @@
+const url = "";
 
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    cont dados = await res.json()
+    console.log(dados);
+}
+
+visualizarInformacoesGlobais();
